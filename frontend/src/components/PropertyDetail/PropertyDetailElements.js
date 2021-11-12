bffdf0e0eec8b2626402591b83b9f0db803518b1
海崎世山 <mtson.20it12@vku.udn.vn>
2021-11-12 17:03:36 +0700

    PropertyDetail

---
+++ frontend/src/components/PropertyDetail/PropertyDetailElements.js
@@ -0,0 +1,186 @@
+import styled from 'styled-components'
+
+export const PropertyOuter = styled.div`
+
+`
+
+export const DetailSection = styled.div`
+    
+`
+
+export const DetailCenter = styled.div`
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0 80px;
+    margin-bottom: 60px;
+`
+
+export const DetailHead = styled.div`
+    display: flex;
+    align-items: flex-start;
+    margin-bottom: 64px;
+    
+`
+
+export const DetailBox = styled.div`
+    flex-grow: 1;
+    padding-right: 40px;
+    margin-top: 150px;
+`
+
+export const DetailTitleH2 = styled.h2`
+    max-width: 640px;
+    margin-bottom: 40px;
+    font-size: 48px;
+    line-height: 1.16667;
+    letter-spacing: -.02em;
+`
+
+export const DetailLine = styled.div`
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+`
+
+export const DetailOpions = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin: -4px 0 6px -20px;
+    @media only screen and (max-width: 1023px){
+        flex: 0 0 100%;
+        margin-top: 6px;
+    }
+`
+
+export const Host = styled.div`
+
+`
+
+export const HostAvatar = styled.div`
+
+`
+
+export const HostAvatarLink = styled.div`
+
+`
+
+export const HostName = styled.h4`
+
+`
+
+export const PropertyType = styled.div`
+
+`
+
+export const PropertyIcon = styled.div`
+
+`
+
+export const PropertyTitle = styled.div`
+
+`
+
+export const DetailGallery = styled.div`
+
+`
+
+export const DetailGalleryList = styled.div`
+
+`
+
+export const DetailGalleryMain = styled.div`
+
+`
+
+export const GalleryMainPreviewImg = styled.div`
+
+`
+
+export const GalleryMainImg = styled.div`
+
+`
+
+export const GalleryMainPreviewButton = styled.div`
+
+`
+
+export const ButtonLink = styled.div`
+
+`
+
+export const DetailGallerySmall = styled.div`
+
+`
+
+export const GallerySmallImg = styled.div`
+
+`
+
+export const DescriptionSection = styled.div`
+
+`
+
+export const DescriptionCenter = styled.div`
+
+`
+
+export const DescriptionWrapper = styled.div`
+
+`
+
+export const DescriptionTextH4 = styled.div`
+
+`
+
+export const DescriptionParameters = styled.div`
+
+`
+
+
+export const DescriptionParametersPart = styled.div`
+
+`
+
+export const Icon = styled.div`
+
+`
+
+export const H5 = styled.div`
+
+`
+
+export const DescriptionContent = styled.div`
+
+`
+
+export const DescriptionContentP = styled.div`
+
+`
+
+export const DescriptionInfo = styled.div`
+
+`
+
+export const DescriptionOptions = styled.div`
+
+`
+export const DetailOptions = styled.div`
+
+`
+export const GallerySmallLink = styled.div`
+
+`
+export const DescriptionOptionPart = styled.div`
+
+`
+
+export const IconModem = styled.div`
+
+`
+
+
+export const Receipt = styled.div`
+
+`
+
