820c80ef2aa23b62e121dc3d53c365e028db90ba
海崎世山 <mtson.20it12@vku.udn.vn>
2021-11-16 02:51:44 +0700

    Property complete

--- frontend/src/components/PropertyDetail/PropertyDetailElements.js
+++ frontend/src/components/PropertyDetail/PropertyDetailElements.js
@@ -97,7 +97,7 @@ export const DetailGalleryList = styled.div`
 `
 
 export const DetailGalleryMain = styled.div`
-    position: relative;
+    
     cursor: pointer;
     grid-column: 1 / 4;
     grid-row: 1 / 4;
@@ -108,11 +108,10 @@ export const GalleryMainPreviewImg = styled.div`
 `
 
 export const GalleryMainImg = styled.img`
-width: 100%;
-height: 100%;
+max-width: 100%;
+max-height: 100%;
 border-radius: 16px;
 object-fit: cover;
-
 `
 
 export const GalleryMainPreviewButton = styled.div`
@@ -186,7 +185,7 @@ display: flex;
 flex-wrap: wrap;
 margin: 24px 0 40px 0;
 padding-top: 14px;
-border-top: 1px solid #E6E8EC;
+border-top: 1px solid #000;
 `
 
 
@@ -194,34 +193,58 @@ export const DescriptionParametersPart = styled.div`
 display: flex;
 align-items: center;
 margin-top: 10px;
-color: #777E90;
+color: #000;
 `
 
 export const Iconbed = styled(BiBed)`
-
+    margin-left: 20px;
+    font-size: 20px;
 `
 export const IconRoom = styled(BiArea)`
+    font-size: 20px;
 `
 
 
 export const H5 = styled.div`
+    font-size: 18px;
+    font-color: #000;
+    margin-left: 5px;
 
 `
 
 export const DescriptionContent = styled.div`
-
+margin-bottom: 48px;
+    font-size: 16px;
+    line-height: 1.5;
 `
 
-export const DescriptionContentP = styled.div`
-
+export const DescriptionContentP = styled.p`
+margin-bottom: 24px;
+color: #878787;
 `
 
-export const DescriptionInfo = styled.div`
-
+export const DescriptionInfo = styled.h2`
+    margin-bottom: 32px;
+    font-size: 24px;
+    line-height: 1.33333;
+    font-weight: 600;
+    letter-spacing: -.01em;
 `
 
 export const DescriptionOptions = styled.div`
-
+position: relative;
+display: flex;
+flex-wrap: wrap;
+margin: -24px 0 48px;
+':before': {
+    content: "";
+    position: absolute;
+    top: 24px;
+    left: 44%;
+    bottom: 0;
+    width: 1px;
+    background: #E6E8EC;
+}
 `
 export const DetailOptions = styled.div`
 
@@ -230,7 +253,11 @@ export const GallerySmallLink = styled.div`
 
 `
 export const DescriptionOptionPart = styled.div`
-
+    display: flex;
+    margin-top: 24px;
+    @media only screen and (max-width: 1339px){
+        padding-right: 24px;
+    }
 `
 
 export const IconModem = styled.div`
@@ -239,6 +266,64 @@ export const IconModem = styled.div`
 
 
 export const Receipt = styled.div`
-
+flex-shrink: 0;
+width: 448px;
+padding: 40px;
+border: 1px solid #fff;
+border-radius: 28px;
+background: #FCFCFD;
+box-shadow: 0px 4px 24px 1px rgba(0, 0, 0, 0.09);
 `
+
+export const ReceiptHead = styled.div``
+export const ReceiptPriceH3 = styled.h3`
+font-family: DM Sans;
+font-style: normal;
+font-weight: bold;
+font-size: 14px;
+line-height: 18px;
+
+color: #878787;
+`
+export const ReceiptPriceNumberH4 = styled.h4`
+font-family: DM Sans;
+font-style: normal;
+font-weight: bold;
+font-size: 48px;
+line-height: 62px;
+
+color: #000000;
+`
+export const ReceiptMiddle = styled.div`
+border-top: 1px solid #878787;
+margin-top: 30px;
+`
+export const Incurred = styled.div``
+export const IncurredH2 = styled.h2`
+font-family: DM Sans;
+font-style: normal;
+font-weight: bold;
+font-size: 24px;
+line-height: 31px;
+margin-top: 15px;
+color: #000000;
+`
+export const ReceiptLine = styled.div`
+    display: flex;
+    padding: 8px 0px;
+`
+export const ReceiptCell = styled.div`
+    margin-right: auto;
+    color: #777E90;
+    `
+export const ReceiptCellRight = styled.div`
+    margin-left: auto;
+    color: #777E90;
+    
+`
+export const ReceiptBtn = styled.div``
+export const BtnStroke = styled.div``
+export const BtnStrokeP = styled.p``
+export const BtnPayment = styled.div``
+export const BtnPaymentP = styled.p``
 
--- frontend/src/components/PropertyDetail/index.js
+++ frontend/src/components/PropertyDetail/index.js
@@ -41,6 +41,20 @@ import {
     DescriptionOptionPart,
     IconModem,
     Receipt,
+    ReceiptHead,
+    ReceiptPriceH3,
+    ReceiptPriceNumberH4,
+    ReceiptMiddle,
+    Incurred,
+    IncurredH2,
+    ReceiptLine,
+    ReceiptCell,
+    ReceiptCellRight,
+    ReceiptBtn,
+    BtnStroke,
+    BtnStrokeP,
+    BtnPayment,
+    BtnPaymentP,
 } from './PropertyDetailElements'
 import Image1 from '../../images/PropertyImage1(1).jpg'
 import Image2 from '../../images/PropertyImage1(2).jpg'
@@ -141,13 +155,10 @@ const PropertyDetail = () => {
 
                         <DescriptionContent>
                             <DescriptionContentP>
-                                figjdwofjoisdjoifj
+                            Designed over 20 to 25 square meters, the intimate room consist of single room featuring a queen-size bed an ensuite bathroom. The spaces are fitted with working stations. Designed with intention in communion with the vibrant neighbourhood, the rooms accomodate up two people 
                             </DescriptionContentP>
                             <DescriptionContentP>
-                                fjkodjfoisdj
-                            </DescriptionContentP>
-                            <DescriptionContentP>
-                                qiweuohjiovgjsiodjgosid
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
                             </DescriptionContentP>
                         </DescriptionContent>
 
@@ -181,7 +192,43 @@ const PropertyDetail = () => {
                         
                     </DescriptionWrapper>
                     <Receipt>
-
+                        <ReceiptHead>
+                            <ReceiptPriceH3>
+                            Price for a month:     
+                            </ReceiptPriceH3>
+                            <ReceiptPriceNumberH4>
+                            $3000
+                            </ReceiptPriceNumberH4>
+                        </ReceiptHead>
+                        <ReceiptMiddle>
+                            <Incurred>
+                            <IncurredH2>Incured</IncurredH2>
+                            <ReceiptLine>
+                                <ReceiptCell>Electricity pricing</ReceiptCell>
+                                <ReceiptCellRight>$102 per kwh</ReceiptCellRight>
+                            </ReceiptLine>
+                            <ReceiptLine>
+                                <ReceiptCell>Watter pricing</ReceiptCell>
+                                <ReceiptCellRight>$30 per m³</ReceiptCellRight>
+                            </ReceiptLine>
+                            <ReceiptLine>
+                                <ReceiptCell>Internet pricing</ReceiptCell>
+                                <ReceiptCellRight>$50</ReceiptCellRight>
+                            </ReceiptLine>
+                            <ReceiptLine>
+                                <ReceiptCell>Vehicle pricing</ReceiptCell>
+                                <ReceiptCellRight>$10</ReceiptCellRight>
+                            </ReceiptLine>
+                            <ReceiptBtn>
+                                <BtnStroke>
+                                    <BtnStrokeP>Save</BtnStrokeP>
+                                </BtnStroke>
+                                <BtnPayment>
+                                    <BtnPaymentP>Reserve</BtnPaymentP>
+                                </BtnPayment>
+                            </ReceiptBtn>
+                            </Incurred>
+                        </ReceiptMiddle>
                     </Receipt>
 
 
