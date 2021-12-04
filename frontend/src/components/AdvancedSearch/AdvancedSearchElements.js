fc4812a1c36ab9efbad198c64e24f8851d3a8a84
海崎世山 <mtson.20it12@vku.udn.vn>
2021-12-05 04:17:19 +0700

    Advanced Search but have bug
    Row column bug

--- frontend/src/components/AdvancedSearch/AdvancedSearchElements.js
+++ frontend/src/components/AdvancedSearch/AdvancedSearchElements.js
@@ -4,6 +4,7 @@ import {BsSearch} from 'react-icons/bs';
 
 export const OuterInner = styled.div`
     display: block;
+
 `
 
 export const CatalogSection = styled.div`
@@ -14,7 +15,7 @@ export const CatalogSection = styled.div`
 
 export const CatalogCenter = styled.div`
     width: 100%;
-    max-width: 1280px;
+    max-width: 1200px;
     margin: 0 auto;
     padding: 0 80px;
     display: block;
@@ -60,27 +61,24 @@ export const CatalogInput = styled.input`
     
 `
 
-export const CatalogResult = styled.div`
-    position: absolute;
-    top: 8px;
-    right: 8px;
-    /* bottom: 8px; */
-    width: 30px;
-    height: 30px;
-    border-radius: 50%;
-    background: #3772FF;
-    transition: background .2s;
-    margin: 0;
-    padding: 0;
-    border: 0;
-    vertical-align: middle;
+export const CatalogResult = styled.button`
+position: absolute;
+top: 8px;
+right: 8px;
+bottom: 8px;
+width: 32px;
+height: 32px;
+border-radius: 50%;
+background: #3772FF;
+transition: background .2s;
+border-width: 0px;
 `
 
 export const SearchIcon = styled(BsSearch)`
     width: 16px;
     height: 16px;
     color: #FCFCFD;
-    vertical-align: text-bottom;
+    
     box-sizing: inherit;
     cursor: pointer;
     overflow: hidden;
@@ -92,12 +90,156 @@ export const SearchIcon = styled(BsSearch)`
 
 
 export const CatalogSorting = styled.div`
-
+    display: flex;
+    align-items: center;
+    margin-bottom: 48px;
 `
+export const Select = styled.select`
+    float: none;
+    width: 100%;
+    height: 48px;
+    padding: 0 48px 0 16px;
+    box-shadow: inset 0 0 0 2px #e6e8ec;
+    background: #FCFCFD;
+    border-radius: 12px;
+    border: none;
+    opacity: 1;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 48px;
+    width: 256px;
+    margin-right: auto;
+    
+`
+export const Option = styled.option`
+font-weight: normal;
+    display: block;
+    white-space: nowrap;
+    min-height: 1.2em;
+    padding: 0px 2px 1px;
+    box-sizing: inherit;
+    `
 
+export const CatalogNav = styled.div`
+display: flex;`
+export const CatalogLink = styled.a`
+    flex-shrink: 0;
+    margin: 0 6px;
+    padding: 6px 12px;
+    border-radius: 14px;
+    background: none;
+    font-family: 'DM Sans', sans-serif;
+    font-size: 14px;
+    line-height: 1.14286;
+    font-weight: 700;
+    color: #777E90;
+    transition: all .2s;
+    &:Hover{
+        background: #353945;
+        color: #FCFCFD;
+        cursor: pointer;
+    }
+`
+export const CatalogLinkActive = styled.a`
+    flex-shrink: 0;
+    margin: 0 6px;
+    padding: 6px 12px;
+    border-radius: 14px;
+    background: none;
+    font-family: 'DM Sans', sans-serif;
+    font-size: 14px;
+    line-height: 1.14286;
+    font-weight: 700;
+    color: #777E90;
+    transition: all .2s;
+    
+        background: #353945;
+        color: #FCFCFD;
+    
+`
 export const CatalogRow = styled.div`
 
 `
 
+export const CatalogFilters = styled.div`
+flex-shrink: 0;
+    width: 256px;`
+export const Range = styled.div`
+`
+export const RangeLabel = styled.h3`
+margin-bottom: 20px;
+font-size: 12px;
+line-height: 1;
+font-weight: 700;
+text-transform: uppercase;
+color: #000;`
+export const CatalogGroup = styled.div`
+margin-bottom: 24px;
+padding: 24px 0;
+border-width: 1px 0;
+border-style: solid;
+border-color: #E6E8EC;
+`
+export const Field = styled.div`
 
+`
+export const FieldLabel = styled.h3`
+margin-top: 12px;
+margin-bottom: 12px;
+font-size: 12px;
+line-height: 1;
+font-weight: 700;
+text-transform: uppercase;
+`
+export const CatalogWrapper = styled.div`
+flex-grow: 1;
+padding-left: 32px;
+`
+
+
+export const CatalogList = styled.div`
+display: flex;
+flex-wrap: wrap;
+margin: -32px -16px 0;
+`
+export const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 0 0 calc(33.333% - 62px);
+    max-width: calc(33.333% - 62px);
+    margin: 32px 16px 0;
+    padding: 12px;
+    box-shadow: 0px 32px 32px rgb(31 47 70 / 12%);
+    border-radius: 20px;
+    @media only screen and (max-width: 1279px){
+        flex: 0 0 calc(50% - 32px);
+        max-width: calc(50% - 32px);
+    }
+`
+export const CardPreview = styled.div`
+position: relative;
+border-radius: 16px;
+overflow: hidden;
+`
+export const CardImg = styled.img`
+vertical-align: middle;
+border-radius: 16px;
+object-fit: cover;
+max-height: 300px;
+width: 100%;
+    transition: -webkit-transform 1s;
+    transition: transform 1s;
+    transition: transform 1s, -webkit-transform 1s;
+`
+export const CardLink = styled.a`
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    padding: 20px 0;
+    color: #23262F;
+    cursor: pointer;
+`
+export const CardH2 = styled.div``
+export const CardCatalog = styled.div``
+export const PriceH3 = styled.div``
 
--- frontend/src/components/AdvancedSearch/index.js
+++ frontend/src/components/AdvancedSearch/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import SliderFilter from './SliderFilter';
+import Image1 from '../../images/PropertyImage1(1).jpg'
 import {
     OuterInner,
     CatalogSection,
@@ -10,9 +12,28 @@ import {
     CatalogResult,
     SearchIcon,
     CatalogSorting,
+    Select,
+    CatalogNav,
+    CatalogLink,
+    CatalogLinkActive,
+    Option,
     CatalogRow,
+    CatalogFilters,
+    Range,
+    RangeLabel,
+    CatalogGroup,
+    Field,
+    FieldLabel,
+    CatalogWrapper,
+    CatalogList,
+    Card,
+    CardPreview,
+    CardImg,
+    CardLink,    
+    CardH2,
+    CardCatalog,
+    PriceH3
 } from './AdvancedSearchElements'
-
 const AdvancedSearch = () => {
     return (
         <OuterInner>
@@ -20,6 +41,7 @@ const AdvancedSearch = () => {
                 <CatalogCenter>
                     <CatalogTop>
                         <CatalogTitle>Advanced Search</CatalogTitle>
+                        
                         <CatalogSearch>
                             <CatalogInput type='text' placeholder='Type something...' />
                             <CatalogResult>
@@ -27,8 +49,78 @@ const AdvancedSearch = () => {
                             </CatalogResult>
                         </CatalogSearch>
                     </CatalogTop>
-                    <CatalogSorting></CatalogSorting>
-                    <CatalogRow></CatalogRow>
+                    <CatalogSorting>
+                        <Select>
+                            <Option>Newest</Option>
+                            <Option>Oldest</Option>
+                        </Select>
+                        <CatalogNav>
+                            <CatalogLinkActive>All</CatalogLinkActive>
+                            <CatalogLink to="#">House</CatalogLink>
+                            <CatalogLink to="#">Studio apartment</CatalogLink>
+                            <CatalogLink to="#">Boarding house</CatalogLink>
+                        </CatalogNav>
+                    </CatalogSorting>
+                    <CatalogRow>
+                        <CatalogFilters>
+                            <Range>
+                                <RangeLabel>Price range</RangeLabel>
+                                <SliderFilter />
+                            </Range>
+                            <CatalogGroup>
+                                <Field>
+                                    <FieldLabel>Price</FieldLabel>
+                                    <Select>
+                                        <Option>Newest</Option>
+                                        <Option>Oldest</Option>
+                                    </Select>
+                                </Field>
+                                <Field>
+                                    <FieldLabel>District</FieldLabel>
+                                    <Select>
+                                        <Option>Newest</Option>
+                                        <Option>Oldest</Option>
+                                    </Select>
+                                </Field>
+                                <Field>
+                                    <FieldLabel>Square</FieldLabel>
+                                    <Select>
+                                        <Option>0 - 30m²</Option>
+                                        <Option>30 - 50m²</Option>
+                                        <Option>50 - 80m²</Option>
+                                        <Option>80 - 100m²</Option>
+                                        <Option>More than 100m²</Option>
+                                    </Select>
+                                </Field>
+                            </CatalogGroup>
+
+                        </CatalogFilters>
+                        <CatalogWrapper>
+                            <CatalogList>
+                                <Card>
+                                    <CardPreview>
+                                        <CardImg src={Image1} />
+                                        <CardLink to="#">
+                                            <CardH2>The modern house</CardH2>
+                                            <CardCatalog>House</CardCatalog>
+                                            <PriceH3>150$</PriceH3>
+                                        </CardLink>
+                                    </CardPreview>
+                                </Card>
+                                <Card>
+                                    <CardPreview>
+                                        <CardImg src={Image1} />
+                                        <CardLink to="#">
+                                            <CardH2>The modern house</CardH2>
+                                            <CardCatalog>House</CardCatalog>
+                                            <PriceH3>150$</PriceH3>
+                                        </CardLink>
+                                    </CardPreview>
+                                </Card>
+                                
+                            </CatalogList>
+                        </CatalogWrapper>
+                    </CatalogRow>
                 </CatalogCenter>
             </CatalogSection>
         </OuterInner>
