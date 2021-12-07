3a6f1ce699f93571e0700fb7a7e5ef698e92b821
Nomddaay <ntnam.20it5@vku.udn.vn>
2021-12-07 12:46:08 +0700

    Category page
    Add image

--- frontend/src/components/Category/CategoryElements.js
+++ frontend/src/components/Category/CategoryElements.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import {TiLocationArrowOutline} from 'react-icons/ti';
-import {BsCalendarRange} from 'react-icons/bs';
+import {
+    BsCalendarRange,
+    BsSearch,
+} from 'react-icons/bs';
 
 export const OuterInner = styled.div`
 
@@ -52,6 +55,7 @@ export const CategoryTitle = styled.div`
     letter-spacing: -.02em;
     font-weight: 700;
     margin-bottom: 240px;
+    color: #ffffff;
 `
 
 export const CategoryPanel = styled.div`
@@ -87,10 +91,9 @@ export const PanelRow = styled.div`
 `
 
 export const Location = styled.div`
-    flex: 0 0 300px;
+    flex: 0 0 310px;
     height: 72px;
     position: relative;
-    z-index: 3;
     overflow: hidden;
 
 `
@@ -119,8 +122,8 @@ export const LocationIcon = styled.div`
 `
 
 export const LocationIconLogo = styled(TiLocationArrowOutline)`
-    width: 24px;
-    height: 24px;
+    width: 30px;
+    height: 30px;
     color: #B1B5C3;
     overflow: hidden;
     vertical-align: middle;
@@ -141,25 +144,247 @@ export const LocationInput = styled.input`
     font-weight: 600;
     letter-spacing: -.01em;
     color: #23262F;
-    cursor: pointer;
+    cursor: auto;
     border: none;
 `
 
 export const CheckIn = styled.div`
-
+    flex: 0 0 280px;
+    height: 72px;
+    position: relative;
+    overflow: hidden;
 `
 
 export const CheckInHead = styled.div`
-
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 16px;
+    user-select: none;
+    transition: box-shadow .2s, background .2s;
 `
 
 export const CheckInIcon = styled.div`
-
+    position: absolute;
+    top: 20px;
+    left: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 32px;
+    height: 32px;
+    pointer-events: none;
 `
 
 export const CalendarLogo = styled(BsCalendarRange)`
+    width: 24px;
+    height: 24px;
+    color: #B1B5C3;
+    overflow: hidden;
+    vertical-align: middle;
+    box-sizing: inherit;
+    position: absolute;
+`
+
+export const CheckInP = styled.p`
+    width: 100%;
+    height: 100%;
+    padding: 0 16px 0 56px;
+    border-radius: 16px;
+    background: none;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    font-size: 22px;
+    line-height: 1.33333;
+    font-weight: 600;
+    letter-spacing: -.01em;
+    color: #23262F;
+    cursor: pointer;
+    border: none;
+    line-height: 72px;
+`
+
+export const CheckOut = styled.div`
+    flex: 0 0 310px;
+    height: 72px;
+    position: relative;
+    overflow: hidden;
+`
+
+export const CheckOutHead = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 16px;
+    user-select: none;
+    transition: box-shadow .2s, background .2s;
+`
+
+export const CheckOutIcon = styled.div`
+    position: absolute;
+    top: 20px;
+    left: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 32px;
+    height: 32px;
+    pointer-events: none;
+`
+
+export const CheckOutP = styled.p`
+    width: 100%;
+    height: 100%;
+    padding: 0 116px 0 56px;
+    border-radius: 16px;
+    background: none;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    font-size: 22px;
+    line-height: 1.33333;
+    font-weight: 600;
+    letter-spacing: -.01em;
+    color: #23262F;
+    cursor: pointer;
+    border: none;
+    line-height: 72px;
+` 
+
+export const PanelSearch = styled.div`
+    position: absolute;
+    top: 50%;
+    right: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 64px;
+    height: 64px;
+    -webkit-transform: translateY(-50%);
+    transform: translateY(-50%);
+    background: #3B71FE;
+    border-radius: 50%;
+    transition: background .2s;
+`
+
+export const PanelSearchIcon = styled.div`
+    vertical-align: middle;
+    box-sizing: inherit;
+    overflow: hidden;
+    cursor: pointer;
+`
+
+export const SearchLogo = styled(BsSearch)`
+    width: 24px;
+    height: 24px;
+    color: #fcfcfd;
+`
+
+export const SectionCatalog = styled.div`
+    margin-bottom: 136px;
+`
+
+export const SectionBrowse = styled.div`
+    margin-bottom: 80px;
+`
+
+export const BrowseCenter = styled.div`
+    width: 100%;
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 0 80px;
+`
+
+export const BrowseInner = styled.div`
+    position: relative;
+`
+
+export const BrowseHead = styled.div`
+    margin-bottom: 48px;
+    padding-right: 120px;
+`
+
+export const BrowseTitle = styled.h4`
+    font-size: 32px;
+    line-height: 1.25;
+    letter-spacing: -.01em;
+    font-weight: 700;
+`
+
+export const BrowseWrapper = styled.div`
+    margin: 0 -16px;
+`
+
+export const BrowseSlider = styled.div`
+    visibility: visible;
+    position: static;
+    display: block;
+    box-sizing: border-box;
+    user-select: none;
+    touch-action: pan-y;
+`
+
+export const SlickList = styled.div`
+    overflow: visible;
+    transform: translate3d(0, 0, 0);
+    position: relative;
+    display: block;
+    margin: 0;
+    padding: 0
+`
+
+export const SlickTrack = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 0 0 calc(33.333% - 62px);
+    max-width: 245px;
+    margin: 32px 16px 0;
+    padding: 12px;
+    box-shadow: 0px 32px 32px rgb(31 47 70 / 12%);
+    border-radius: 20px;
+
+    @media only screen and (max-width: 1279px){
+        flex: 0 0 calc(50% - 32px);
+        max-width: calc(50% - 32px);
+    }
+`
+
+export const CardPreview = styled.div`
+    position: relative;
+    border-radius: 16px;
+    overflow: hidden;
+`
+ 
+export const CardImg = styled.img`
+    vertical-align: middle;
+    border-radius: 16px;
+    object-fit: cover;
+    max-height: 250px;
+    max-width: 250px;
+    transition: -webkit-transform 1s;
+    transition: transform 1s;
+    transition: transform 1s, -webkit-transform 1s;
+`
+
+export const CardLink = styled.div`
 
 `
+
+export const CardH2 = styled.h2`
+
+`
+
+
+export const PriceH3 = styled.div`
+
+`
+
+
 
 
 
